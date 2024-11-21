7bad2063d58f3ad4b89c44453de7f1d635dfc372
Walaafadol <walaafadul7@gmail.com>
2024-11-21 18:49:04 +0000

    comm

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Rotate 2D Matrix
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """rotate two dimension matrix 90 degrees clockwise
+    Args:
+        matrix (list[[list]]): a matrix
+    """
+
+    n = len(matrix)
+
+    # Step 1: Transpose the matrix
+    for i in range(n):
+        for j in range(i + 1, n):  # Only swap upper triangle elements
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    # Step 2: Reverse each row
+    for i in range(n):
+        matrix[i].reverse()
